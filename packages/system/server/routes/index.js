14ea48fb60a2054f1b7687e988349aeb6bc2db44
ahanlon1987 <ahanlon@gmail.com>
2014-09-23 14:08:22 -0500

    Simple print out of single user's time logged

--- packages/system/server/routes/index.js
+++ packages/system/server/routes/index.js
@@ -3,7 +3,7 @@
 module.exports = function(System, app, auth, database) {
 
 //  var http = require('http');
-  var JiraApi = require("jira").JiraApi;
+  var JiraApi = require('jira').JiraApi;
 
   var config = {
         'user': 'jirabot',
@@ -20,24 +20,22 @@ module.exports = function(System, app, auth, database) {
 
 
   // Worklog route, in the future this will return data from JIRAs API
+  //TODO move this work into a service class
   app.route('/worklog')
       .get(function(req, res, next){
 
-          var query = 'sprint in openSprints()';
+          var query = 'targetUser=steved';
           var jira = new JiraApi('https', config.host, config.port, config.user, config.password, '2');
 
 
-          jira.searchJira(query, {fields:["summary", "issuekey", "status",
-              "assignee", "timeestimate",
-              "workratio","aggregatetimeoriginalestimate",
-              "aggregatetimeestimate", "aggregatetimespent"]}, function(error, issues) {
+          jira.searchWorklog(query,function(error, issues) {
 
               if(error){
                   console.log(error);
                   return;
               }
 
-              console.log(issues);
+//              console.log(issues);
               res.json(issues);
 
 
