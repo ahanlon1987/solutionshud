7b762fcef612b0ce968f4827d81441faf58312b4
ahanlon1987 <ahanlon@gmail.com>
2014-09-23 10:15:34 -0500

    added simple AJAX call

--- packages/system/public/controllers/worklog.js
+++ packages/system/public/controllers/worklog.js
@@ -1,7 +1,21 @@
 'use strict';
 
-angular.module('mean.system').controller('WorklogController', ['$scope', 'Global',
-  function($scope, Global) {
-    $scope.global = Global;
+angular.module('mean.system').controller('WorklogController', ['$scope', '$http', 'Global',
+  function($scope, $http, Global) {
+
+      $scope.global = Global;
+      $scope.myData = {};
+      $scope.myData.doClick = function(item, event) {
+
+          var responsePromise = $http.get('/worklog');
+
+          responsePromise.success(function(data, status, headers, config) {
+              $scope.myData.fromServer = data;
+          });
+          responsePromise.error(function(data, status, headers, config) {
+              alert('AJAX failed!');
+          });
+      };
+
   }
 ]);
