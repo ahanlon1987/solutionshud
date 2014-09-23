24120fbca34d1ffcc8ce45f4aabd108e17eee796
ahanlon1987 <ahanlon@gmail.com>
2014-09-23 10:28:24 -0500

    Printing dummy data from JIRA

--- packages/system/public/controllers/worklog.js
+++ packages/system/public/controllers/worklog.js
@@ -4,13 +4,13 @@ angular.module('mean.system').controller('WorklogController', ['$scope', '$http'
   function($scope, $http, Global) {
 
       $scope.global = Global;
-      $scope.myData = {};
-      $scope.myData.doClick = function(item, event) {
+      $scope.worklogData = {};
+      $scope.worklogData.doClick = function(item, event) {
 
           var responsePromise = $http.get('/worklog');
 
           responsePromise.success(function(data, status, headers, config) {
-              $scope.myData.fromServer = data;
+              $scope.worklogData = data;
           });
           responsePromise.error(function(data, status, headers, config) {
               alert('AJAX failed!');
